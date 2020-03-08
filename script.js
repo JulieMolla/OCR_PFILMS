26ae3ac5c3a285bf94d82d8a044d9f0d5debff89
Julie Molla <jm@MacBook-Pro-de-Julie.local>
2020-03-08 11:48:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    // Transition effect for navbar 
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > $(this).height()) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
+    });
+});
+
+
+
+
+$(document).ready(function () {
+
+    // Gets the video src from the data-src on each button
+
+    var $videoSrc;
+    $('.video-btn').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
+
+
+
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function (e) {
+
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    })
+
+
+
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src', $videoSrc);
+    })
+
+
+
+
+
+
+    // document ready  
+});
+
+
