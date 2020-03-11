1373f42af6cf049f2a1de9be5c7340347d18e583
Julie MOLLA <julie.molla@union-habitat.org>
2020-03-11 14:06:12 +0100

    refact code js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Fonctions en Javascript
+
+//Fonction permettant l'affichage de la navbar: elle sera fixe sur l'ensemble du site, transparente sur la première partie du site puis opaque
 $(document).ready(function () {
     // Transition effect for navbar 
     $(window).scroll(function () {
@@ -10,42 +13,19 @@ $(document).ready(function () {
     });
 });
 
-
-
-
+// Fonction permettant d'insérer les modal bootstrap (pour l'affichage des vidéos)
 $(document).ready(function () {
-
-    // Gets the video src from the data-src on each button
-
     var $videoSrc;
     $('.video-btn').click(function () {
         $videoSrc = $(this).data("src");
     });
     console.log($videoSrc);
 
-
-
-    // when the modal is opened autoplay it  
     $('#myModal').on('shown.bs.modal', function (e) {
-
-        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
     })
 
-
-
-    // stop playing the youtube video when I close the modal
     $('#myModal').on('hide.bs.modal', function (e) {
-        // a poor man's stop video
         $("#video").attr('src', $videoSrc);
     })
-
-
-
-
-
-
-    // document ready  
 });
-
-
